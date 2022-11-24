7edded8c77cec31c37abe2d0839600df5b5ef1e7
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-11-24 23:51:13 +0200

    Fixes bugs

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -2,7 +2,7 @@ import { statusFilters } from './constants';
 // Импортируем функцию композиции редюсеров
 import { combineReducers } from 'redux';
 
-const taskInitialState = [
+const tasksInitialState = [
   { id: 0, text: 'Learn HTML and CSS', completed: true },
   { id: 1, text: 'Get good at JavaScript', completed: true },
   { id: 2, text: 'Master React', completed: false },
@@ -11,80 +11,45 @@ const taskInitialState = [
 ];
 
 // Используем initialState как значение состояния по умолчанию
-export const taskReducer = (state = taskInitialState, action) => {
-  // Редюсер различает экшены по значению свойства type
+const tasksReducer = (state = tasksInitialState, action) => {
   switch (action.type) {
-    // В зависимости от типа экшена будет выполняться разная логика
-    // Нужно вернуть новый объект состояния
-    // в котором есть все данные существующего состояния
-    // и новый массив задач
-    // в котором есть все существующие задачи
-    // и объект новой задачи
-    case 'tasks/addTask': {
-      return {
-        ...state,
-        tasks: [...state.tasks, action.payload],
-      };
-    }
+    case 'tasks/addTask':
+      return [...state, action.payload];
     case 'tasks/deleteTask':
-      return {
-        ...state,
-        tasks: state.tasks.filter(task => task.id !== action.payload),
-      };
+      return state.filter(task => task.id !== action.payload);
     case 'tasks/toggleCompleted':
-      return {
-        ...state,
-        tasks: state.tasks.map(task => {
-          if (task.id !== action.payload) {
-            return task;
-          }
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }),
-      };
+      return state.map(task => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+        return { ...task, completed: !task.completed };
+      });
     default:
-      // Каждый редюсер получает все экшены отправленные в стор.
-      // Если редюсер не должен обрабатывать какой-то тип экшена,
-      // необходимо вернуть существующее состояние без изменений.
       return state;
   }
 };
-
 const filtersInitialState = {
   status: statusFilters.all,
 };
 
-export const filterReducer = (state = filtersInitialState, action) => {
+const filtersReducer = (state = filtersInitialState, action) => {
   switch (action.type) {
     case 'filters/setStatusFilter':
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      };
-
+      return { ...state, status: action.payload };
     default:
-      // Каждый редюсер получает все экшены отправленные в стор.
-      // Если редюсер не должен обрабатывать какой-то тип экшена,
-      // необходимо вернуть существующее состояние без изменений.
       return state;
   }
 };
-
 // export const rootReducer = (state = {}, action) => {
 //   // Возвращаем объект состояния
 //   return {
 //     // Обоим редюсерам передаем только часть состояния за которую они отвечают
-//     tasks: taskReducer(state.tasks, action),
-//     filters: filterReducer(state.filters, action),
+//     tasks: tasksReducer(state.tasks, action),
+//     filters: filtersReducer(state.filters, action),
 //   };
 // };
 
 export const rootReducer = combineReducers({
-  tasks: taskReducer,
-  filters: filterReducer,
+  tasks: tasksReducer,
+  filters: filtersReducer,
 });
