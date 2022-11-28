2c0a28f14780cc4d45d09d8dd0ecaf8c545dcb2c
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-11-28 18:00:56 +0200

    Can't persist

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,14 +1,11 @@
-// Импортируем хук
 import { useSelector } from 'react-redux';
 import { getTasks } from 'redux/selectors';
-
 import css from './TaskCounter.module.css';
 
 export const TaskCounter = () => {
-  // Получаем массив задач из состояния Redux
   const tasks = useSelector(getTasks);
+  console.log('TASKS', tasks);
 
-  // На базе состояния Redux получаем производные данные
   const count = tasks.reduce(
     (acc, task) => {
       if (task.completed) {
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,42 +3,19 @@ import { tasksReducer } from './tasksSlice';
 import { filtersReducer } from './filtersSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 const persistTasksConfig = {
   key: 'tasks',
   storage,
 };
 
-const persistFiltersConfig = {
-  key: 'filters',
-  storage,
-};
-
-const persistedTasksReducer = persistReducer(persistTasksConfig, tasksReducer);
-const persistedFiltersReducer = persistReducer(
-  persistFiltersConfig,
-  filtersReducer
-);
+const persistedReducer = persistReducer(persistTasksConfig, tasksReducer);
 
 export const store = configureStore({
   reducer: {
-    tasks: persistedTasksReducer,
-    filters: persistedFiltersReducer,
+    tasks: persistedReducer,
+    filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
 // export const persistor = persistStore(store);
